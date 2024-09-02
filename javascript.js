104b2762f4a10fbf5fa3f88ab8666d200d8e03c4
若鷺伊万里 <ttaiga1324@gmail.com>
2024-09-02 18:54:13 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -3,3 +3,5 @@ console.log("信濃眞伊のウェブサイトへようこそ");
 console.log("特に面白味もありませんが、ごゆっくりどうぞ");
 console.log("ちなみに、");
 console.log("信濃眞伊はJavascriptがかけません");
+console.log("Javascript.js更新日");
+console.log("皇紀2684年9月2日月曜日");
