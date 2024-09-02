bc020785887da9f4f1a7111f5e6419842f308dbb
若鷺伊万里 <ttaiga1324@gmail.com>
2024-09-02 18:55:10 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -5,3 +5,5 @@ console.log("ちなみに、");
 console.log("信濃眞伊はJavascriptがかけません");
 console.log("Javascript.js更新日");
 console.log("皇紀2684年9月2日月曜日");
+console.log("Whitten by SHINANOMAI");
+console.log("CopyRight© 2022-2024(皇紀2682-2684) SHINANOMAI All rights reserved.");
